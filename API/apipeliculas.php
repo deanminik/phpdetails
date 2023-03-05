973702a252887e66486695be41a3356ab291a4a3
Dean <deaneryfb@gmail.com>
2023-03-04 23:15:47 -0600

    API with parameter

--- API/apipeliculas.php
+++ API/apipeliculas.php
@@ -2,20 +2,22 @@
 
 include_once 'pelicula.php';
 
-class ApiPeliculas{
+class ApiPeliculas
+{
 
 
-    function getAll(){
+    function getAll()
+    {
         $pelicula = new Pelicula();
         $peliculas = array();
         $peliculas["items"] = array(); // el objecto items se convierte en arreglo de objetos
 
         $res = $pelicula->obtenerPeliculas();
 
-        if($res->rowCount()){//es mayor que cero?
-            while ($row = $res->fetch(PDO::FETCH_ASSOC)){
-    
-                $item=array(
+        if ($res->rowCount()) { //es mayor que cero?
+            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+
+                $item = array(
                     "id" => $row['id'],
                     "nombre" => $row['nombre'],
                     "imagen" => $row['imagen'],
@@ -23,14 +25,52 @@ class ApiPeliculas{
                 array_push($peliculas["items"], $item);
                 // array_push para insertar arreglos dentro de un arreglo 
             }
-        
-            echo json_encode($peliculas);
-            //convierte en JSON el arreglo de objetos
-        }else{
-            echo json_encode(array('mensaje' => 'No hay elementos'));
+
+            // echo json_encode($peliculas);
+            $this->printJSON($peliculas);
+            // convierte en JSON el arreglo de objetos
+        } else {
+            // echo json_encode(array('mensaje' => 'No hay elementos'));
             // json_encode parsea o convierte en json lo que quiero mostrar 
+            $this->error('No hay elementos registrados');
+        }
+    }
+    function error($mensaje)
+    {
+        echo json_encode(array('mensaje' => $mensaje));
+    }
+
+    function printJSON($array)
+    {
+        echo '<code>' . json_encode($array) . '</code>';
+    }
+
+    function getById($id)
+    {
+        $pelicula = new Pelicula();
+        $peliculas = array();
+        $peliculas["items"] = array(); // el objecto items se convierte en arreglo de objetos
+
+        $res = $pelicula->obtenerPeliculaById($id);
+
+        if ($res->rowCount()) {
+            $row = $res->fetch();
+            $item = array(
+                "id" => $row['id'],
+                "nombre" => $row['nombre'],
+                "imagen" => $row['imagen'],
+            );
+            array_push($peliculas["items"], $item);
+            // array_push para insertar arreglos dentro de un arreglo 
+
+
+            // echo json_encode($peliculas);
+            $this->printJSON($peliculas);
+            // convierte en JSON el arreglo de objetos
+        } else {
+            // echo json_encode(array('mensaje' => 'No hay elementos'));
+            // json_encode parsea o convierte en json lo que quiero mostrar 
+            $this->error('No hay elementos registrados');
         }
     }
 }
-
-?>
--- API/pelicula.php
+++ API/pelicula.php
@@ -8,6 +8,11 @@ class Pelicula extends DB{
         $query = $this->connect()->query('SELECT * FROM peliculas');
         return $query;
     }
+    function obtenerPeliculaById($id){
+        $query = $this->connect()->prepare('SELECT * FROM peliculas WHERE id = :id');
+        $query->execute(['id' => $id]);
+        return $query;
+    }
 
 }
 
