1c83162cb855dedd907d55b08dd9b55b42ed39f0
Dean <deaneryfb@gmail.com>
2023-06-13 19:47:48 -0600

    Even and odd true or false

--- practice/even-odd.php
+++ practice/even-odd.php
@@ -8,4 +8,19 @@ function even_or_odd(int $n): string
 }
 
 
-echo even_or_odd(8);
+// echo even_or_odd(8);
+
+
+function lovefunc($flower1, $flower2)
+{
+    // moment of truth
+
+    $flower1 = $flower1 % 2 ? "Odd" : "Even";
+    $flower2 = $flower2 % 2 ? "Odd" : "Even";
+
+    $result =  $flower1 != $flower2 ? true : false;
+
+    return  $result;
+}
+
+echo lovefunc(4, 1);
